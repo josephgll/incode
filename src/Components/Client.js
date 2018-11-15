b130834cf7c73b57e4013c6a69ac0c7736036cfd
Gleb Amelichkin <glebamelichkin@gmail.com>
2018-11-15 10:37:54 +0200

    client.js

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -4,7 +4,16 @@ import Clients from '../clients.json';
 class Client extends React.Component{
   render(){
     return(
-      
+      <div class="ui celled list selection">
+  <div class="item">
+    <img class="ui avatar image" src={Clients[0].general.avatar} />
+    <div class="content">
+      <div class="header">{Clients[0].general.firstName} {Clients[0].general.lastName}</div>
+      {Clients[0].job.title}
+    </div>
+  </div>
+</div>
+
     )
   }
 }
