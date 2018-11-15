07963528dd7560b44e10ca82a4907394489b67af
Gleb Amelichkin <glebamelichkin@gmail.com>
2018-11-15 10:16:10 +0200

    start

---
+++ src/Components/Client.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clients from '../clients.json';
+
+class Client extends React.Component{
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default Client;
